e945c88c2cd81f5a2773de303979526a4bb7dabb
Vinnu Gollakoti <vinnugollakoti@Vinnus-MacBook-Pro.local>
2026-03-12 16:50:28 +0530

    added

--- server/routes/class.ts
+++ server/routes/class.ts
@@ -50,4 +50,62 @@ router.post("/create-class", auth, async(req: AuthRequest, res: Response) => {
 });
 
 
+router.post("/create-subject", auth, async (req: AuthRequest, res: Response) => {
+    try {
+        if ((req.user.role) !== "PRINCIPAL" &&  req.user.role !== "RECEPTIONIST" && req.user.role !== "TEACHER") {
+            return res.status(400).json({message : "UnAuthorized area"});
+        }
+
+        const {name, classId} = req.body;
+
+        if (!name || !classId) {
+            return res.status(500).json({message: "You missed required inputs"});
+        }
+
+        let subject = await prisma.subject.findUnique({
+            where: {name}
+        })
+
+        if (!subject) {
+            subject = await prisma.subject.create ({
+                data: { name }
+            })
+        }
+
+        const existing = await prisma.classSubject.findUnique({
+            where : {
+                classId_subjectId: {
+                    classId, subjectId: subject.id
+                }
+            }
+        });
+
+        if (existing) {
+            return res.status(400).json({
+                message: "Subject already existed and assigned to class";
+            })
+        }
+
+        const classSubject = await prisma.classSubject.create({
+            data: {
+                classId,
+                subjectId: subject.id
+            },
+
+            include: {
+                class: true,
+                subject: true
+            }
+        });
+
+        res.json({message: "Subject created successfully", data: classSubject})
+
+    } catch(err) {
+        console.log(err)
+        return res.status(400).json({message: "Failed to create subject."})
+    }
+})
+
+// router.post("/create-exam")
+
 export default router;
